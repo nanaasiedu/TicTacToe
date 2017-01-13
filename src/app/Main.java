fab1f2f24eaf1501323c577843cd66006cc6a20a
Nana <na1814@ic.ac.uk>
2017-01-13 00:32:40 +0000

    Added initial implementation using JavaFx

---
+++ src/app/Main.java
@@ -0,0 +1,43 @@
+package app;
+
+import app.controllers.MainWindowController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+        mainWindow();
+    }
+
+    public void mainWindow() {
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("/app/views/mainView.fxml"));
+            AnchorPane pane = loader.load();
+
+            MainWindowController mainWindowController = loader.getController();
+
+            Scene scene = new Scene(pane);
+
+            primaryStage.setScene(scene);
+
+            primaryStage.show();
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/app/controllers/MainWindowController.java
@@ -0,0 +1,10 @@
+package app.controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainWindowController {
+    @FXML
+    public void openPlayerSelectScene(ActionEvent event) {
+        System.out.println("HELLO WORLD");
+    }
+}
