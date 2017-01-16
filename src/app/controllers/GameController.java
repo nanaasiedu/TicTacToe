9e5eeddeef37d08d917f51c345b7ad593fb6446d
Nana <na1814@ic.ac.uk>
2017-01-16 00:50:45 +0000

    Small grammar change

--- src/app/controllers/GameController.java
+++ src/app/controllers/GameController.java
@@ -8,7 +8,7 @@ public class GameController extends AbstractController {
     public static int GRID_SPAN = 1;
     public static String HUMAN_TEXT = "(Human) pick a square";
     public static String AI_TEXT = "(AI) is thinking...";
-    public static String WRONG_MOVE_TEXT = "please pick a empty square!";
+    public static String WRONG_MOVE_TEXT = "Please pick a empty square!";
     public static String ENG_GAME_TEXT = "GAME FINISHED! AND THE WINNER IS : ";
 
     private Label gameLabel;
